bdfd5c6e2462b72dc7db7c4833adde4312940ab4
Italo Costa <italocostajobs@gmail.com>
2021-08-18 21:45:36 -0300

    feat: animando com aos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Cards from './components/Cards';
 import Form from './components/Form';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
+  AOS.init({
+    anchorPlacement: 'top-center',
+    duration: 600,
+  });
+
   return (
     <div className="App">
       <Navbar />
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -4,19 +4,19 @@ export default function Cards() {
   return (
     <div id="about" className="container">
       <ul>
-        <li>
+        <li data-aos="fade-left">
           <h1>Aprendar a finalidade de uma resenha</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li>
+        <li data-aos="fade-up">
           <h1>Entenda a sua estrutura</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </li>
-        <li>
+        <li data-aos="fade-right">
           <h1>Dicas para escrever-las com mais facilidade</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
