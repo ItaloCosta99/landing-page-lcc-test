9c65bae84c072f2ae21a9f8f57cd513d50eec5a3
Italo Costa <italocostajobs@gmail.com>
2021-08-18 22:44:54 -0300

    fix: animação do botão de download acontece apenas uma vez

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="hero-text">
         <h3>Esse e-book foi preparado com as melhores dicas para realizar suas resenhas.</h3>
       </div>
-      <div className="download" data-aos="fade-up" data-aos-anchor-placement="top-center">
+      <div className="download" data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-once="true">
         <a href="/">Download</a>
         <FaArrowCircleLeft color="#00fa43" size="30" />
       </div>
